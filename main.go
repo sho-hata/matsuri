c8e32593699c3164ccc4e32f22cb0ed05300c216
sho-hata <syouki100241@gmail.com>
2022-07-27 00:22:33 +0900

    implement repl

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/user"
+
+	"github.com/sho-hata/matsuri/repl"
+)
+
+func main() {
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Hello %s!! This is the Monkey programming language!\n", user.Username)
+
+	fmt.Println("feel free to type in commands")
+	repl.Start(os.Stdin, os.Stdout)
+}
