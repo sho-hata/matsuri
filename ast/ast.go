43da71afbd8875dbc9edb5cee37a79687d4c8776
sho-hata <syouki100241@gmail.com>
2022-07-27 19:10:53 +0900

    implement return statement parser

--- ast/ast.go
+++ ast/ast.go
@@ -20,6 +20,7 @@ type Program struct {
 	Statements []Statement
 }
 
+// let <identifier> = <expression>;
 type LetStatement struct {
 	Token token.Token
 	Name  *Identifier
@@ -31,6 +32,19 @@ var _ Statement = (*LetStatement)(nil)
 func (ls *LetStatement) statementNode() {}
 
 func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
+
+// return <expression>;
+
+type ReturnStatement struct {
+	Token       token.Token // 'return' token
+	ReturnValue Expression
+}
+
+var _ Statement = (*ReturnStatement)(nil)
+
+func (rs *ReturnStatement) statementNode() {}
+
+func (rs *ReturnStatement) TokenLiteral() string { return rs.Token.Literal }
 
 type Identifier struct {
 	Token token.Token
--- parser/parser.go
+++ parser/parser.go
@@ -49,9 +49,24 @@ func (p *Parser) parseStatement() ast.Statement {
 	switch p.currentToken.Type {
 	case token.LET:
 		return p.parseLetStatement()
+	case token.RETURN:
+		return p.parseReturnStatement()
 	default:
 		return nil
 	}
+}
+
+func (p *Parser) parseReturnStatement() *ast.ReturnStatement {
+	stmt := &ast.ReturnStatement{Token: p.currentToken}
+
+	p.nextToken()
+
+	// TODO: Avoid skipping to the semicolon.
+	for !p.currentTokenIs(token.SEMICOLON) {
+		p.nextToken()
+	}
+
+	return stmt
 }
 
 func (p *Parser) parseLetStatement() *ast.LetStatement {
